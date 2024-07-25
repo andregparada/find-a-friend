76d0a83cc0ff325f9a49822b3fb65cd571f2742f
André <andregparada@gmail.com>
2024-07-25 15:38:49 -0300

    feat: more register use case tests

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,6 +1,8 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
 import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { OrgAlreadyExistsError } from './errors/org-already-exists-error'
 
 let orgsRepository: InMemoryOrgsRepository
 let sut: RegisterUseCase
@@ -27,5 +29,59 @@ describe('Register Use Case', () => {
     })
 
     expect(org.id).toEqual(expect.any(String))
+  })
+
+  it('should be able to hash org password upon registration', async () => {
+    const { org } = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+      phone_number: '21987654321',
+      cep: '20200200',
+      state: 'EX',
+      city: 'Example City',
+      neighborhood: 'Example Neighborhood',
+      street: 'Example Street',
+      latitude: -7.9437058,
+      longitude: -14.3607169,
+    })
+
+    const isPasswordCorrectlyHashed = await compare('123456', org.password_hash)
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const email = 'johndoe@example.com'
+
+    await sut.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+      phone_number: '21987654321',
+      cep: '20200200',
+      state: 'EX',
+      city: 'Example City',
+      neighborhood: 'Example Neighborhood',
+      street: 'Example Street',
+      latitude: -7.9437058,
+      longitude: -14.3607169,
+    })
+
+    await expect(() =>
+      sut.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+        phone_number: '21987654321',
+        cep: '20200200',
+        state: 'EX',
+        city: 'Example City',
+        neighborhood: 'Example Neighborhood',
+        street: 'Example Street',
+        latitude: -7.9437058,
+        longitude: -14.3607169,
+      }),
+    ).rejects.toBeInstanceOf(OrgAlreadyExistsError)
   })
 })
