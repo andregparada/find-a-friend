1cf40e419a6e8afad61cb6958f479cbfd7a29eaa
André <andregparada@gmail.com>
2024-07-25 14:55:02 -0300

    feat: in memory orgs repository

---
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -0,0 +1,39 @@
+import { Org, Prisma } from '@prisma/client'
+import { OrgsRepository } from '../orgs-repository'
+import { randomUUID } from 'crypto'
+
+export class InMemoryOrgsRepository implements OrgsRepository {
+  public items: Org[] = []
+
+  create(data: Prisma.OrgCreateInput) {
+    const org = {
+      id: randomUUID().toString(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      phone_number: data.phone_number,
+      created_at: new Date(),
+      cep: data.cep,
+      state: data.state,
+      city: data.city,
+      neighborhood: data.neighborhood,
+      street: data.street,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+    }
+
+    this.items.push(org)
+
+    return org
+  }
+
+  findByEmail(email: string) {
+    const org = this.items.find((item) => item.email === email)
+
+    if (!org) {
+      return null
+    }
+
+    return org
+  }
+}
--- src/repositories/prisma/prisma-orgs-repository.ts
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -2,6 +2,10 @@ import { Prisma, Org } from '@prisma/client'
 import { OrgsRepository } from '../orgs-repository'
 
 export class PrismaOrgsRepository implements OrgsRepository {
+  findByEmail(email: string): Promise<Org | null> {
+    throw new Error('Method not implemented.')
+  }
+
   create(data: Prisma.OrgCreateInput): Promise<Org> {
     throw new Error('Method not implemented.')
   }
