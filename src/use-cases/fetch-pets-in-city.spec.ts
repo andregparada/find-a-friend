1b4cd9ed58850e18f796ed8c06379409c33f5b08
André <andregparada@gmail.com>
2024-08-02 11:41:04 -0300

    feat: filtragem no fetch pets

--- src/use-cases/fetch-pets-in-city.spec.ts
+++ src/use-cases/fetch-pets-in-city.spec.ts
@@ -15,7 +15,7 @@ describe('Fetch Pets In City Use Case', () => {
     sut = new FetchPetsInCityUseCase(petsRepository, orgsRepository)
   })
 
-  it('should be able to fetch pets in the city', async () => {
+  it('should be able to fetch pets from different orgs in the city', async () => {
     const orgOneId = await orgsRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
@@ -124,6 +124,101 @@ describe('Fetch Pets In City Use Case', () => {
     expect(pets).toEqual([
       expect.objectContaining({ name: 'Pet 21' }),
       expect.objectContaining({ name: 'Pet 22' }),
+    ])
+  })
+
+  it('should be able to fetch filtered pets', async () => {
+    const orgId = await orgsRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+      phone_number: '21987654321',
+      cep: '20200200',
+      state: 'EX',
+      city: 'Example City',
+      neighborhood: 'Example Neighborhood',
+      street: 'Example Street',
+      latitude: -7.9437058,
+      longitude: -14.3607169,
+    })
+
+    await petsRepository.create({
+      org_id: orgId.id,
+      name: 'Small and Puppy Pet 1',
+      species: 'Cat',
+      race: 'Mutt',
+      sex: 'MALE',
+      color: ['Orange'],
+      characteristics: 'Fun',
+      age: 'PUPPY',
+      size: 'SMALL',
+      energy_level: 'ONE',
+      independence_level: 'LOW',
+      environment_needs: 'SMALL',
+      adoption_requirements: ['None'],
+    })
+
+    await petsRepository.create({
+      org_id: orgId.id,
+      name: 'Small and Puppy Pet 2',
+      species: 'Cat',
+      race: 'Mutt',
+      sex: 'MALE',
+      color: ['Orange'],
+      characteristics: 'Fun',
+      age: 'PUPPY',
+      size: 'SMALL',
+      energy_level: 'ONE',
+      independence_level: 'LOW',
+      environment_needs: 'SMALL',
+      adoption_requirements: ['None'],
+    })
+
+    await petsRepository.create({
+      org_id: orgId.id,
+      name: 'Medium Pet',
+      species: 'Cat',
+      race: 'Mutt',
+      sex: 'MALE',
+      color: ['Orange'],
+      characteristics: 'Fun',
+      age: 'PUPPY',
+      size: 'MEDIUM',
+      energy_level: 'ONE',
+      independence_level: 'LOW',
+      environment_needs: 'SMALL',
+      adoption_requirements: ['None'],
+    })
+
+    await petsRepository.create({
+      org_id: orgId.id,
+      name: 'Adult Pet',
+      species: 'Cat',
+      race: 'Mutt',
+      sex: 'MALE',
+      color: ['Orange'],
+      characteristics: 'Fun',
+      age: 'ADULT',
+      size: 'MEDIUM',
+      energy_level: 'ONE',
+      independence_level: 'LOW',
+      environment_needs: 'SMALL',
+      adoption_requirements: ['None'],
+    })
+
+    const { pets } = await sut.execute({
+      city: 'Example City',
+      page: 1,
+      size: 'SMALL',
+      age: 'PUPPY',
+    })
+
+    console.log(pets)
+
+    expect(pets).toHaveLength(2)
+    expect(pets).toEqual([
+      expect.objectContaining({ name: 'Small and Puppy Pet 1' }),
+      expect.objectContaining({ name: 'Small and Puppy Pet 2' }),
     ])
   })
 })
--- src/use-cases/fetch-pets-in-city.ts
+++ src/use-cases/fetch-pets-in-city.ts
@@ -3,9 +3,18 @@ import { PetsRepository } from '../repositories/pets-repository'
 import { OrgsRepository } from '@/repositories/orgs-repository'
 import { OrgNotFoundError } from './errors/org-not-found-error'
 
+type Age = 'PUPPY' | 'ADULT' | 'SENIOR'
+type Size = 'SMALL' | 'MEDIUM' | 'LARGE'
+type EnergyLevel = 'ONE' | 'TWO' | 'THREE' | 'FOUR' | 'FIVE'
+type IndependenceLevel = 'LOW' | 'MEDIUM' | 'HIGH'
+
 interface FetchPetsInCityUseCaseRequest {
   city: string
   page: number
+  age?: Age
+  size?: Size
+  energy_level?: EnergyLevel
+  independence_level?: IndependenceLevel
 }
 
 interface FetchPetsInCityUseCaseResponse {
@@ -21,6 +30,10 @@ export class FetchPetsInCityUseCase {
   async execute({
     city,
     page,
+    age,
+    energy_level,
+    size,
+    independence_level,
   }: FetchPetsInCityUseCaseRequest): Promise<FetchPetsInCityUseCaseResponse> {
     const orgsInCity = await this.orgsRepository.findManyByCity(city)
 
@@ -28,15 +41,30 @@ export class FetchPetsInCityUseCase {
       throw new OrgNotFoundError()
     }
 
-    const pets: Pet[] = []
+    let pets: Pet[] = []
 
-    orgsInCity.map(async (org) => {
-      const orgPets = await this.petsRepository.findManyByOrgId(org.id, page)
+    const allPets = await Promise.all(
+      orgsInCity.map(async (org) => {
+        return this.petsRepository.findManyByOrgId(org.id, page)
+      }),
+    )
 
-      orgPets.forEach((pet) => {
-        pets.push(pet)
-      })
-    })
+    pets = allPets.flat()
+
+    console.log('Before filtering:', pets)
+
+    if (age) {
+      pets = pets.filter((pet) => pet.age === age)
+    }
+    if (energy_level) {
+      pets = pets.filter((pet) => pet.energy_level === energy_level)
+    }
+    if (size) {
+      pets = pets.filter((pet) => pet.size === size)
+    }
+    if (independence_level) {
+      pets = pets.filter((pet) => pet.independence_level === independence_level)
+    }
 
     return {
       pets,
