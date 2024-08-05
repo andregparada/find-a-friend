cee3dfa1e1cd69d8190a3d3d1fdbf875ea675aaf
André <andregparada@gmail.com>
2024-08-05 15:24:08 -0300

    feat: delete pet use case

---
+++ src/use-cases/delete-pet.ts
@@ -0,0 +1,20 @@
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { PetsRepository } from '@/repositories/pets-repository'
+
+interface DeleteOrgUseCaseRequest {
+  id: string
+}
+
+export class DeleteOrgUseCase {
+  constructor(private petsRepository: PetsRepository) {}
+
+  async execute({ id }: DeleteOrgUseCaseRequest) {
+    const pet = await this.petsRepository.findById(id)
+
+    if (!pet) {
+      throw new ResourceNotFoundError()
+    }
+
+    await this.petsRepository.delete(pet)
+  }
+}
