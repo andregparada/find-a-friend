babf8a2d8621f355992480b028d8c12fc183d3ae
André <andregparada@gmail.com>
2024-08-05 15:01:06 -0300

    feat: prisma repository create

--- src/repositories/prisma/prisma-orgs-repository.ts
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -1,12 +1,33 @@
 import { Prisma, Org } from '@prisma/client'
 import { OrgsRepository } from '../orgs-repository'
+import { prisma } from '@/lib/prisma'
 
 export class PrismaOrgsRepository implements OrgsRepository {
-  findByEmail(email: string): Promise<Org | null> {
+  async create(data: Prisma.OrgCreateInput) {
+    const org = await prisma.org.create({
+      data,
+    })
+
+    return org
+  }
+
+  async update(data: Prisma.OrgUpdateInput): Promise<Org> {
     throw new Error('Method not implemented.')
   }
 
-  create(data: Prisma.OrgCreateInput): Promise<Org> {
+  async delete(id: string): void {
+    throw new Error('Method not implemented.')
+  }
+
+  async findById(id: string): Promise<Org | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findByEmail(email: string): Promise<Org | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  async findManyByCity(city: string): Promise<Org[]> {
     throw new Error('Method not implemented.')
   }
 }
