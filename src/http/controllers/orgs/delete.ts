81460e1b13be133cde8430aac79e7b129d6cbdad
André <andregparada@gmail.com>
2024-08-07 15:53:06 -0300

    feat: delete org controller

--- src/http/controllers/orgs/delete.ts
+++ src/http/controllers/orgs/delete.ts
@@ -1,42 +1,12 @@
-import { makeUpdateOrgUseCase } from '@/use-cases/factories/make-update-org-use-case'
+import { makeDeleteOrgUseCase } from '@/use-cases/factories/make-delete-org-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { z } from 'zod'
 
 export async function update(request: FastifyRequest, reply: FastifyReply) {
-  const deleteBodySchema = z.object({ orgId: z.string() })
+  const deleteOrg = makeDeleteOrgUseCase()
 
-  const {
-    name,
-    email,
-    password,
-    phone_number,
-    cep,
-    state,
-    city,
-    neighborhood,
-    street,
-    latitude,
-    longitude,
-  } = registerBodySchema.parse(request.body)
-
-  const updateOrg = makeUpdateOrgUseCase()
-
-  const { updatedOrg } = await updateOrg.execute({
+  await deleteOrg.execute({
     orgId: request.user.sub,
-    name,
-    email,
-    password,
-    phone_number,
-    cep,
-    state,
-    city,
-    neighborhood,
-    street,
-    latitude,
-    longitude,
   })
 
-  return reply.status(200).send({
-    updatedOrg,
-  })
+  return reply.status(204).send()
 }
