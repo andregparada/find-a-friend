edaf2a87c4a107a25e95edf9b01c52e50ea6354f
André <andregparada@gmail.com>
2024-08-08 14:04:51 -0300

    feat: get pet details controller

---
+++ src/http/controllers/pets/get-pet-details.ts
@@ -0,0 +1,24 @@
+import { makeGetPetProfileUseCase } from '@/use-cases/factories/make-get-pet-profile-use-case'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+export async function getPetDetails(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const createPetParamSchema = z.object({
+    petId: z.string().uuid(),
+  })
+
+  const { petId } = createPetParamSchema.parse(request.params)
+
+  const getPetProfileUseCase = makeGetPetProfileUseCase()
+
+  const pet = await getPetProfileUseCase.execute({
+    petId,
+  })
+
+  return reply.status(200).send({
+    pet,
+  })
+}
