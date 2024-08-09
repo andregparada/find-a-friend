d7f17d7c8b380fe369519055fe760d96368627bf
André <andregparada@gmail.com>
2024-08-09 15:28:13 -0300

    fix: refresh token spec

--- src/http/controllers/orgs/refresh.spec.ts
+++ src/http/controllers/orgs/refresh.spec.ts
@@ -1,7 +1,6 @@
 import request from 'supertest'
 import { app } from '@/app'
 import { describe, it, expect, beforeAll, afterAll } from 'vitest'
-import { hash } from 'bcryptjs'
 
 describe('Refresh Token (e2e)', () => {
   beforeAll(async () => {
@@ -13,21 +12,19 @@ describe('Refresh Token (e2e)', () => {
   })
 
   it('should be able to refresh a token', async () => {
-    await request(app.server)
-      .post('/orgs')
-      .send({
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password_hash: await hash('123456', 6),
-        phone_number: '21987654321',
-        cep: '20200200',
-        state: 'EX',
-        city: 'Example City',
-        neighborhood: 'Example Neighborhood',
-        street: 'Example Street',
-        latitude: -7.9437058,
-        longitude: -14.3607169,
-      })
+    await request(app.server).post('/orgs').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+      phone_number: '21987654321',
+      cep: '20200200',
+      state: 'EX',
+      city: 'Example City',
+      neighborhood: 'Example Neighborhood',
+      street: 'Example Street',
+      latitude: -7.9437058,
+      longitude: -14.3607169,
+    })
 
     const authResponse = await request(app.server).post('/sessions').send({
       email: 'johndoe@example.com',
