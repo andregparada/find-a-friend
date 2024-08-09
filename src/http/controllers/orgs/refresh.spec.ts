ed0697fcee540abae58a7bdac776bc225cd3a255
André <andregparada@gmail.com>
2024-08-09 15:03:38 -0300

    feat: refresh token

---
+++ src/http/controllers/orgs/refresh.spec.ts
@@ -0,0 +1,52 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import { hash } from 'bcryptjs'
+
+describe('Refresh Token (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to refresh a token', async () => {
+    await request(app.server)
+      .post('/orgs')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password_hash: await hash('123456', 6),
+        phone_number: '21987654321',
+        cep: '20200200',
+        state: 'EX',
+        city: 'Example City',
+        neighborhood: 'Example Neighborhood',
+        street: 'Example Street',
+        latitude: -7.9437058,
+        longitude: -14.3607169,
+      })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const cookies = authResponse.get('Set-Cookie') ?? []
+
+    const response = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+    expect(response.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken='),
+    ])
+  })
+})
--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -4,10 +4,13 @@ import { authenticate } from './authenticate'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { update } from './update'
 import { deleteOrg } from './delete'
+import { refresh } from './refresh'
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/orgs', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.put('/update', { onRequest: [verifyJWT] }, update)
   app.delete('/delete', { onRequest: [verifyJWT] }, deleteOrg)
